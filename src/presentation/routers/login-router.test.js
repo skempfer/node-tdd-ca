246a4d5d9c6c40264d0069c3042d6734b4471975
skempfer <shana.kempfer88@gmail.com>
2020-10-18 20:09:14 -0300

    test: ensure LoginRouter returns 400 if no passpword is provided

--- src/presentation/routers/login-router.test.js
+++ src/presentation/routers/login-router.test.js
@@ -1,6 +1,6 @@
 class LoginRouter {
   route (httpRequest) {
-    if (!httpRequest.body.email) {
+    if (!httpRequest.body.email || !httpRequest.body.password) {
       return {
         statusCode: 400
       }
@@ -14,6 +14,17 @@ describe('login router', () => {
     const httpRequest = {
       body: {
         password: '123456'
+      }
+    }
+    const httpResponse = sut.route(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+  })
+
+  test('Should return 400 if no password is provided', () => {
+    const sut = new LoginRouter()
+    const httpRequest = {
+      body: {
+        email: 'any@gmail.com'
       }
     }
     const httpResponse = sut.route(httpRequest)
